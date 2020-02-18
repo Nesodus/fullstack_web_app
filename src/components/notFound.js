7119fd9e3b5d126ef3dbdee037388ae2d012ba20
Ilya Kobelev <ilykobelev@gmail.com>
2020-02-18 22:01:04 +0300

    initial commit

---
+++ src/components/notFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const notFound = () =>
+    <div>
+        <h3>404 Page (Sadly) Not Found</h3>
+        <p>Woops, looks like something broke :(</p>
+    </div>;
+
+
+export default notFound();
